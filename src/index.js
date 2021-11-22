cedbad34af5cc2ea10e3f369ee9dbef449a1664a
Joy <greeniejoy@gmail.com>
2021-11-22 16:53:57 -0600

    added seed.js

--- src/index.js
+++ src/index.js
@@ -4,3 +4,8 @@ import App from './App';
 
 ReactDOM.render(<App />,document.getElementById('root'));
 
+// client side rendered app: react (create-react-app)
+//  connect to database which is Firebase
+// dependancies: react-loading-skeleton
+// styling: tailwind
+
